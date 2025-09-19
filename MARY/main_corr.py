62595e8f26ea143862aa949472470d9e53f13e66
julianssvr <julianssvr@gmail.com>
2025-09-18 23:34:27 -0500

    Update

--- MARY/main_corr.py
+++ MARY/main_corr.py
@@ -1,28 +1,33 @@
+import os
+import dotenv
 from frqi_mary_corr import FRQI_MARY_CORR_Simulator
 
 if __name__ == "__main__":
-    # Define las intensidades de los píxeles (ej. para una imagen 2x2)
+    # Define las intensidades de los píxeles
     my_intensities = [20, 60, 120, 240]
 
     # Crea una instancia del simulador
     n_shots = 8192 #8192
     simulator = FRQI_MARY_CORR_Simulator(intensities=my_intensities, shots=n_shots)
 
+    # cargamos las variables de entorno
+    dotenv.load_dotenv()  
+
     # --- Conexión a una computadora cuántica real de IBM (Actualizado 2025) ---
-    my_api_token = "6_qSMCprV_-VjF3prep9iooxdmBQIPXdUANfz9h9Z7gH" # Reemplaza con tu token real de IBM Cloud
+    my_api_token = os.getenv('my_api_token') # Reemplaza con tu token real de IBM Cloud
     
     # Opcional: Proporciona tu instancia (CRN) para optimizar la búsqueda de backends.
     # Si no la pones, Qiskit la buscará por ti.
-    my_instance = "crn:v1:bluemix:public:quantum-computing:us-east:a/f1d7b6cfa7094a2c8c9b845ca9d29dad:d1467f7d-3360-4d28-8fef-9025fea31956::"
+    my_instance = os.getenv('my_instance')
 
     # Conectar al backend de IBM usando el nuevo canal y las credenciales actualizadas
-    simulator.connect_to_ibm_backend(
-        token=my_api_token,
-        backend_name="ibm_torino", # O el nombre del backend que prefieras, o None
+    #simulator.connect_to_ibm_backend(
+    #    token=my_api_token,
+    #    backend_name="ibm_torino", # O el nombre del backend que prefieras, o None
         # El canal 'ibm_quantum' está obsoleto. Se usa 'ibm_quantum_platform'.
         #Para correr en el simulador poner por favor 
-        channel="ibm_quantum_platform"
-    )
+    #    channel="ibm_quantum_platform"
+    #)
 
     print("\nEJECUTANDO CIRCUITO CUÁNTICO CON COMPUERTAS MARY")
     simulator.run()
